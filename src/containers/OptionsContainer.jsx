5c4a54eef7ca6be6ef7394963df3b9cd26fc5c90
gerretkubota <gerretkubota@gmail.com>
2018-10-10 11:33:52 -0700

    In progress with Filter component

--- src/containers/OptionsContainer.jsx
+++ src/containers/OptionsContainer.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-
+import Filter from '../components/Filter.jsx';
 export default class OptionsContainer extends Component {
   render() {
     return (
       <div id="options-container">
         hasldjfalsdjf
+        <Filter />
       </div>
     )
   }
