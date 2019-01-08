59e7cec2c056907960bb59efb58fdccd9e5466d6
Patrick Slagle <slaglebagel17@gmail.com>
2019-01-07 18:30:04 -0600

    linted file

--- src/containers/navbar.jsx
+++ src/containers/navbar.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 
-const NavBar = ({ handleApp }) => {
-  return (
-    <div id="navbar">
-      <div id="nav-title">
-        <h1>CSDoor</h1>
-      </div>
-      <div id='nav-buttons'>
-        <button
-          className='nav-button'
-          value='post'
-          onClick={handleApp}
-          type='button'
-        >
-          Post
-        </button>
-        <button className='nav-button' value='home' onClick={handleApp}>Home</button>
-        {/* <button value='profile'>Profile</button> */}
-      </div>
+const NavBar = ({ handleApp }) => (
+  <div id="navbar">
+    <div id="nav-title">
+      <h1>CSDoor</h1>
     </div>
-  )
-}
+    <div id="nav-buttons">
+      <button
+        className="nav-button"
+        value="post"
+        onClick={handleApp}
+        type="button"
+      >
+        Post
+      </button>
+      <button
+        className="nav-button"
+        value="home"
+        onClick={handleApp}
+        type="button"
+      >
+        Home
+      </button>
+      {/* <button value="profile">Profile</button> */}
+    </div>
+  </div>
+);
 
-export default NavBar; 
+
+export default NavBar;
