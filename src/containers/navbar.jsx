eda5642f6e260e9f0b176578a57f8d638ebc729d
Patrick Slagle <slaglebagel17@gmail.com>
2019-01-07 18:28:30 -0600

    made component stateless

--- src/containers/navbar.jsx
+++ src/containers/navbar.jsx
@@ -1,24 +1,25 @@
-import React, { Component } from 'react'
-import FlatButton from 'material-ui/FlatButton';
+import React from 'react';
 
-class NavBar extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div id="navbar">
-        <div id="nav-title">
-          <h1>CSDoor</h1>
-        </div>
-        <div id='nav-buttons'>
-          <button className='nav-button' value='post' onClick={this.props.handleApp}>Post</button>
-          <button className='nav-button' value='home' onClick={this.props.handleApp}>Home</button>
-          {/* <button value='profile'>Profile</button> */}
-        </div>
+const NavBar = ({ handleApp }) => {
+  return (
+    <div id="navbar">
+      <div id="nav-title">
+        <h1>CSDoor</h1>
       </div>
-    )
-  }
+      <div id='nav-buttons'>
+        <button
+          className='nav-button'
+          value='post'
+          onClick={handleApp}
+          type='button'
+        >
+          Post
+        </button>
+        <button className='nav-button' value='home' onClick={handleApp}>Home</button>
+        {/* <button value='profile'>Profile</button> */}
+      </div>
+    </div>
+  )
 }
 
 export default NavBar; 
